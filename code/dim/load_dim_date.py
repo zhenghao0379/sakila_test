2732a2b5083113b5dc29c7afd6669b46a93cf7ef
zhenghao <dpzhenghao@dfl.com.cn>
2020-03-30 16:52:43 +0800

    update

---
+++ code/dim/load_dim_date.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import datetime
+import time
+
+df_index = pd.date_range(datetime.date(2000,1,1), datetime.date(2030,1,1))
+
+df_date = pd.DataFrame()
+
+df_date["date"] = df_index.strftime("%F")
+df_date["year"] = df_index.strftime("%Y")
+df_date["month"] = df_index.strftime("%m")
+df_date["day"] = df_index.strftime("%d")
+df_date["week"] = df_index.strftime("%w")
+df_date["week"] = df_date["week"].replace("0", "7")
+df_date["month_full_name"] = df_index.strftime("%B")
+df_date["month_short_name"] = df_index.strftime("%b")
+df_date["week_full_name"] = df_index.strftime("%A")
+df_date["week_short_name"] = df_index.strftime("%a")
+
+
+
+
+
+

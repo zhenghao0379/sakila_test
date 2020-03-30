4cc13a01aeadb7761574af08284f20ffb7d63c38
zhenghao <dpzhenghao@dfl.com.cn>
2020-03-30 22:18:22 +0800

    update

--- code/dim/load_dim_date.py
+++ code/dim/load_dim_date.py
@@ -1,20 +1,61 @@
 import numpy as np
+import os, sys
 import pandas as pd
 import datetime
-import  from package
+import calendar
+import pendulum
+# import  from package
+
+path = sys.path[0]
+print(path)
+
+
+
+
+# 自定义函数
+def week_in_month(date_value):
+    dt = pendulum.parse(date_value)
+    return int(dt.week_of_month)
+
+
+def quarter_number(x):
+    if '01' <= x <= '03':
+        return '01'
+    elif '04' <= x <= '06':
+        return '02'
+    elif '07' <= x <= '09':
+        return '03'
+    elif '10' <= x <= '12':
+        return '04'
+    else:
+        return 'error'
 
 df_index = pd.date_range(datetime.date(2000,1,1), datetime.date(2030,1,1))
 
 df_date = pd.DataFrame()
 
-df_date["date"] = df_index.strftime("%F")
-df_date["year"] = df_index.strftime("%Y")
-df_date["month"] = df_index.strftime("%m")
-df_date["day"] = df_index.strftime("%d")
-df_date["week"] = df_index.strftime("%w")
-df_date["week"] = df_date["week"].replace("0", "7")
-df_date["month_full_name"] = df_index.strftime("%B")
-df_date["month_short_name"] = df_index.strftime("%b")
-df_date["week_full_name"] = df_index.strftime("%A")
-df_date["week_short_name"] = df_index.strftime("%a")
+df_date["date_key"] = df_index.strftime("%Y%m%d")
+df_date["date_value"] = df_index.strftime("%F")
+df_date["date_short"] = df_index.strftime("%D")
+df_date["date_medium"] = df_index.strftime("%b %d, %Y")
+df_date["date_long"] = df_index.strftime("%B %d, %Y")
+df_date["date_full"] = df_index.strftime("%A, %B %d, %Y")
+df_date["day_in_year"] = df_index.strftime("%j")
+df_date["day_in_month"] = df_index.strftime("%d")
+df_date["day_abbreviation"] = df_index.strftime("%a")
+df_date["day_name"] = df_index.strftime("%A")
+df_date["week_in_year"] = df_index.strftime("%W")
+df_date["week_in_month"] = df_date["date_value"].apply(week_in_month)
+df_date["is_first_in_week"] = df_date["day_abbreviation"].apply(lambda x: 'yes' if x == 'Mon' else 'no')
+df_date["is_last_in_week"] = df_date["day_abbreviation"].apply(lambda x: 'yes' if x == 'Sun' else 'no')
+df_date["month_number"] = df_index.strftime("%m")
+df_date["month_abbreviation"] = df_index.strftime("%b")
+df_date["month_name"] = df_index.strftime("%B")
+df_date["year2"] = df_index.strftime("%y")
+df_date["year4"] = df_index.strftime("%Y")
+df_date["quarter_name"] = df_date["month_number"].apply(quarter_number) + 'Q'
+df_date["quarter_number"] = df_date["month_number"].apply(quarter_number)
+df_date["year_quarter"] = df_date["year4"] + '-' + df_date["quarter_name"]
+df_date["year_month_number"] = df_index.strftime("%Y-%m")
+df_date["year_month_abbreviation"] = df_index.strftime("%Y-%b")
 
